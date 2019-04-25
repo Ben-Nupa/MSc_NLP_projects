1e2f327047730dd0df54a5cab31c1bf14e00b995
Benoit Laurès <benoit.laures@student.ecp.fr>
2019-04-25 19:59:52 +0200

    fixed reading files

--- exercise_1/tools.py
+++ exercise_1/tools.py
@@ -11,14 +11,14 @@ def display_data(data, number_of_lines=5):
 
 
 def read_dataset(
-        path_to_dataset_folder='data/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled',
+        path_to_file='1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled/news.en-00001-of-00100',
         number_lines=10 ** 6):
     """
-    Read the given dataset.
+    Read the given file.
 
     Parameters
     ----------
-    path_to_dataset_folder : str
+    path_to_file : str
         Relative path.
     number_lines : int
         Lines to be extracted (10**6 by default)
@@ -29,39 +29,26 @@ def read_dataset(
         List of sentences where sentences are list of words.
     """
 
-    # Getting files in the dataset folder
-    files = glob.glob(path_to_dataset_folder + '/*')
-
-    # Sorting to be sure to have alphabetical order
-    files.sort()
-
-    # Printing number of found files
-    # print("Found", str(len(files)), "files to read.")
-
     # Init regex to remove non alphanumerical characters, still we keep spaces and '
     regex = re.compile("[^a-z ']+")
 
     data = []
-    id_file = 0
-    while len(data) < number_lines:
-        with open(files[id_file], encoding='utf-8') as current_file:
-            # print("Opening file n°" + str(id_file) + ". Wrote " + str(len(data)) + "/" + str(number_lines))
+    with open(path_to_file, encoding='utf-8') as current_file:
+        # print("Opening file n°" + str(id_file) + ". Wrote " + str(len(data)) + "/" + str(number_lines))
 
-            # Init line
+        # Init line
+        line = current_file.readline()
+
+        while line and len(data) < number_lines:
+            # While we are not in the end of the file and we haven't filled the data list
+
+            # Removing unwanted characters
+            processed_line = regex.sub('', line.lower())
+            final_line = processed_line.replace("  ", " ").split(" ")
+            final_line.pop()
+            data.append(final_line)
+
+            # refresh line value
             line = current_file.readline()
-
-            while line and len(data) < number_lines:
-                # While we are not in the end of the file and we haven't filled the data list
-
-                # Removing unwanted characters
-                processed_line = regex.sub('', line.lower())
-                final_line = processed_line.replace("  ", " ").split(" ")
-                final_line.pop()
-                data.append(final_line)
-
-                # refresh line value
-                line = current_file.readline()
-        # Moving to new file
-        id_file += 1
-    # print("Finished reading")
+    # print("Finished reading with " + str(len(data)) + " lines.")
     return data
